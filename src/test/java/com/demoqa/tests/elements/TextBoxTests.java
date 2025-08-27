3d29e3985968f7a419ecb54881a3110636327552
irinabaker <paxpistor@gmail.com>
2025-08-27 20:33:35 +0200

    Updated

--- src/test/java/com/demoqa/tests/elements/TextBoxTests.java
+++ src/test/java/com/demoqa/tests/elements/TextBoxTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvFileSources;
 
 public class TextBoxTests extends TestBase {
 
